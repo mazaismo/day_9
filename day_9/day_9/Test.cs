541ff18952ce42bfdd44d17e0b33d0d66f6b39b0
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-17 19:44:51 +0300

    jauna clase

---
+++ day_9/day_9/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day_9
+{
+    class Test
+    {
+        // public, private
+        public int Saskaitisana(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void Izvade()
+        {
+            Console.WriteLine("tests");
+        }
+    }
+}
